4f13e5bd740ddcd2cc7636aae1b9d63e26740a91
vasha <vasha@drpa-develop.umcc.cu>
2022-11-14 07:30:38 -0500

    Subida inicial

---
+++ lang/es/theme_eguru.php
@@ -0,0 +1,162 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Strings for component 'theme_eguru', language 'en'
+ *
+ * @package   theme_eguru
+ * @copyright 2015 LMSACE Dev Team, lmsace.com
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$string['about'] = 'Acerca de';
+$string['aboutus'] = 'Acerca de nosotros';
+$string['pluginname'] = 'Eguru';
+$string['address'] = 'Direcci�n';
+$string['choosereadme'] = '<div class="clearfix"><div class="theme_screenshot">
+<img class=img-polaroid src="eguru/pix/screenshot.jpg" />
+<h3>Theme Credits</h3>
+<p>
+<h3>Moodle Eguru theme</h3>
+<p>
+This theme is based on the Boost based Moodle theme.
+</p>
+<p>
+Authors: LMSACE Dev Team<br>
+Contact: info@lmsace.com<br>
+Website: <a href="http://www.lmsace.com">www.lmsace.com</a><br>
+</p>';
+$string['configtitle'] = 'Eguru';
+$string['connectus'] = 'Conectate con nosotros';
+$string['contact'] = 'Contacto';
+$string['customcss'] = 'css personalizado';
+$string['customcssdesc'] = 'Sea cual sea CSS reglas se agrega a esta �rea de texto se reflejar� en cada p�gina, para hacer m�s f�cil la personalizaci�n de este tema.';
+$string['defaultaddress'] = '308 Negra Narrow Lane, Albeeze, New york, 87104';
+$string['defaultemailid'] = 'info@example.com';
+$string['defaultphoneno'] = '(000) 123-456';
+$string['emailid'] = 'Email';
+$string['fburl'] = 'Facebook';
+$string['fburl_default'] = 'https://www.facebook.com/yourfacebookid';
+$string['fburldesc'] = 'La url de Facebook de su organizaci�n.';
+$string['pcourses'] = 'Cursos promocionados';
+$string['pcoursesdesc'] = 'Por favor dar el id cursos promovidos debe separados por comas.';
+$string['promotedcoursesheading'] = 'Cursos promocionados';
+$string['footerheading'] = 'Bloques de pie de p�gina';
+$string['footnote'] = 'Nota al pie';
+$string['footnotedefault'] = 'Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno est�ndar de las industrias desde el a�o 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido us� una galer�a de textos y los mezcl� de tal manera que logr� hacer un libro de textos especimen. No s�lo sobrevivi� 500 a�os, sino que tambien ingres� como texto de relleno en documentos electr�nicos.';
+$string['footnotedesc'] = 'Lo que se agrega a esta �rea de texto se mostrar� en el pie a trav�s de su Moodle site.Please dan la clave sea la lengua o text.For ex: lang:display or Display';
+$string['frontpageheading'] = 'primera plana';
+$string['gpurl'] = 'Google+';
+$string['gpurl_default'] = 'https://www.google.com/+yourgoogleplusid';
+$string['gpurldesc'] = 'La url Google+ de su organizaci�n.';
+$string['headerheading'] = 'Cabecera';
+$string['logo'] = 'logotipo';
+$string['logodesc'] = 'Por favor, cargue su logotipo personalizado aqu� si quieres a�adirlo a la cabecera.
+                       <br>La imagen debe ser 50px alto y cualquier anchura razonable (m�nimo: 235px) que trajes.';
+$string['numberofslides'] = 'N�mero de diapositivas';
+$string['numberofslides_desc'] = 'N�mero de diapositivas sobre el control deslizante.';
+$string['numberoftmonials'] = 'N�mero de Testimonios.';
+$string['numberoftmonials_desc'] = 'N�mero de Testimonios en la P�gina de Inicio.';
+$string['phoneno'] = 'Telefono no';
+$string['pinurl'] = 'Pinterest';
+$string['pinurl_default'] = 'https://in.pinterest.com/yourpinterestname/';
+$string['pinurldesc'] = 'La url Pinterest de su organizaci�n.';
+$string['knowmore'] = 'Saber m�s';
+$string['region-side-post'] = 'Correcto';
+$string['region-side-pre'] = 'Izquierda';
+$string['signup'] = 'contratar';
+$string['slidecaption'] = 'leyenda de diapositivas';
+$string['slidecaptiondefault'] = 'Moodle Learning Management System';
+$string['slidecaptiondesc'] = 'Introduzca el texto de la leyenda que se utilizar� para la diapositiva';
+$string['slideimage'] = 'imagen Diapositiva';
+$string['slideimagedesc'] = 'La imagen debe ser 1366px X 385px.';
+$string['slideno'] = 'Diapositiva {$a->slide}';
+$string['slidenodesc'] = 'Introduzca los ajustes de diapositivas {$a->slide}.';
+$string['slideshowdesc'] = 'Esto crea una presentaci�n de diapositivas de hasta doce diapositivas para usted para promover elementos importantes de su sitio. El espect�culo es sensible en altura de la imagen se ajusta de acuerdo a la pantalla tama�o.Si se selecciona ninguna imagen para una diapositiva, entonces se utiliza las im�genes default_slide en la carpeta pix.';
+$string['slideshowheading'] = 'P�gina de inicio deslizante.';
+$string['slideshowheadingsub'] = 'Presentaci�n de la primera p�gina.';
+$string['slideurl'] = 'enlace de diapositivas.';
+$string['slideurldesc'] = 'Introduzca el destino objetivo del v�nculo de la imagen de diapositivas.';
+$string['toggleslideshow'] = 'Presentaci�n de diapositivas pantalla.';
+$string['toggleslideshowdesc'] = 'Elija si desea ocultar o mostrar la presentaci�n de diapositivas.';
+$string['twurl'] = 'Twitter';
+$string['twurl_default'] = 'https://twitter.com/yourtwittername';
+$string['twurldesc'] = 'La url de Twitter de su organizaci�n.';
+$string['themegeneralsettings'] = 'General';
+$string['patternselect'] = 'Esquema de color del sitio';
+$string['patternselectdesc'] = 'Seleccione el esquema de color que desea tener para su sitio.';
+$string['colorscheme'] = 'Esquema de colores';
+$string['footerblock'] = 'pie de p�gina Bloquear';
+$string['title'] = 'T�tulo';
+$string['footerbtitle_desc'] = 'Por favor, dar el t�tulo o bien tecla de idioma bloque pie de p�gina o text.For ex: lang:display or Display';
+$string['footerbtitle2default'] = 'enlaces rapidos';
+$string['footerbtitle3default'] = 'S�ganos';
+$string['footerbtitle4default'] = 'Contacto';
+$string['footerblink'] = 'Pie de p�gina Bloquear Enlace';
+$string['footerblink2default'] = 'lang:aboutus|http://www.example.com/about-us.php
+lang:termsofuse|http://www.example.com/terms-of-use.php
+lang:faq|http://www.example.com/faq.php
+lang:support|http://www.example.com/support.php
+lang:contact|http://www.example.com/contact.php';
+$string['footerblink_desc'] = 'Puede configurar un pie de p�gina Bloquear Links aqu� para ser mostrado por temas. Cada l�nea se compone de un texto del men� o bien tecla de idioma o el texto, un link URL (opcional), separados por tuber�a characters.For ejemplo:
+<pre>
+lang:moodlecommunity|https://moodle.org
+Moodle Support|https://moodle.org/support
+</pre>';
+$string['medianame1'] = 'Facebook';
+$string['medianame2'] = 'Twitter';
+$string['medianame3'] = 'Google Plus';
+$string['medianame4'] = 'Pinterest';
+
+$string['mediaicon1'] = 'fa-facebook-f';
+$string['mediaicon2'] = 'fa-twitter';
+$string['mediaicon3'] = 'fa-google-plus';
+$string['mediaicon4'] = 'fa-pinterest-p';
+$string['default_color'] = 'Esquema de color por defecto';
+$string['color_schemes_heading'] = 'Combinaciones de colores';
+$string['promotedtitledefault'] = 'cursos promocionados';
+$string['promotedtitledesc'] = 'Por favor, dar el t�tulo de bloque cursos promovidos, cualquiera de las teclas idioma o Text.
+For ex: lang:display or Display';
+$string['pcourseenable'] = 'Habilitar cursos promocionados';
+$string['login'] = 'Iniciar sesi�n';
+$string['link'] = 'v�nculo';
+$string['text'] = 'Texto';
+$string['icon'] = 'Icono';
+$string['loginheader'] = 'Accede a tu cuenta';
+$string['marketingspotsheading'] = 'manchas de marketing';
+$string['marketingspot'] = 'punto de comercializaci�n';
+$string['marketingspot1desc'] = 'Introduzca el 1 bloque de contenido, ya sea tecla de idioma de Marketing del punto o texto here.For ex: lang:display or Display';
+$string['marketingspot2desc'] = 'Introduzca el 2 bloque de contenido, ya sea tecla de idioma de Marketing del punto o texto here.For ex: lang:display or Display';
+$string['mspottitledesc'] = 'Introduzca el marketing Punto T�tulo cualquier tecla de idioma o Text.For ex: lang:display or Display';
+$string['mspotdescdesc'] = 'Introduzca el marketing Descripci�n del punto ya sea lenguaje clave o Text.For ex: lang:display or Display';
+$string['mspot1titledefault'] = 'pluriling�e';
+$string['mspot1descdefault'] = 'Esta es la versi�n de Photoshop de Lorem Ipsum. Proin gravida nibh vel Velit auctor aliquet. Sollicitudin Aenean, lorem quis bibendum auctor, nisi elit consequat.';
+$string['mspot2titledefault'] = 'Estudio en l�nea';
+$string['mspot2descdefault'] = 'TEsta es la versi�n de Photoshop de Lorem Ipsum. Proin gravida nibh vel Velit auctor aliquet. Sollicitudin Aenean, lorem quis bibendum auctor, nisi elit consequat.';
+$string['mspot3titledefault'] = 'Apoyo a la Comunidad';
+$string['mspot3descdefault'] = 'Esta es la versi�n de Photoshop de Lorem Ipsum. Proin gravida nibh vel Velit auctor aliquet. Sollicitudin Aenean, lorem quis bibendum auctor, nisi elit consequat.';
+$string['mspot4titledefault'] = 'Dise�o de respuesta';
+$string['mspot4descdefault'] = 'Esta es la versi�n de Photoshop de Lorem Ipsum. Proin gravida nibh vel Velit auctor aliquet. Sollicitudin Aenean, lorem quis bibendum auctor, nisi elit consequat.';
+$string['faicondesc'] = 'Introduzca el nombre del icono que desea utilizar. Lista es <a href="http://fortawesome.github.io/Font-Awesome/cheatsheet/" target="_new">here</a>. Solo tienes que introducir lo que es despu�s de la "fa-".';
+$string['slideurltext'] = 'Slide link text';
+$string['slideurltextdesc'] = 'Introduzca el destino objetivo del texto deslizante enlace de imagen,
+cualquiera de las teclas idioma o Text.For ex: lang:display or Display';
+$string['termsofuse'] = 'Condiciones de uso';
+$string['faq'] = 'PF';
+$string['support'] = 'apoyo';
+$string['copyright'] = '<p class = "text-center"> Derechos de autor y copia; 2015 - Desarrollado por
+������������ <a href="http://www.lmsace.com/"> LMSACE.com </a> .Powered por <a href="https://moodle.org"> Moodle </a> </ p>';
+$string['footeremail'] = 'Correo:';
